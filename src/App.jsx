4720920aea11f90eecd11644de245e9b018ebbac
MITHUN BAADKAR <98984237+MithunBaadkar@users.noreply.github.com>
2024-12-05 09:27:40 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./BlocksDifs/HeaderDifs/Header"
+import { Body } from "./BlocksDifs/BodyDifs/Body"
+
+function App() {
+  return(
+    <>
+      <Header />
+      <Body />
+    </>
+    
+  )
+}
+
+export default App
